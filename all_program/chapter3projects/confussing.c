c950d055b728dddb01b00f7e5715473f6cc07db4
arvind7770 <arvindpatidar7771@gmail.com>
2024-11-04 19:15:23 +0530

    simple but most confussing program

---
+++ all_program/chapter3projects/confussing.c
@@ -0,0 +1,17 @@
+// this is so simple program but some times students consfuse in this program
+#include <stdio.h>
+
+int main(){
+    int a=5;
+    // if we use a==11 . but we write if(a=11)
+    // it means value of a replace to 11
+    if (a=11)
+    {
+        printf("value of a is 11");
+    }
+    else
+    {
+        printf("value of a is 5");
+    }
+    return 0;
+}
