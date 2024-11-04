853acde407e32d04ea21f55342971145badfceee
arvind7770 <arvindpatidar7771@gmail.com>
2024-11-04 13:50:38 +0530

    write a code for understanding. How OR Operator Works

--- all_program/chapter3/OR_Operator.c
+++ all_program/chapter3/OR_Operator.c
@@ -1,6 +1,23 @@
+// In this program we will learn use or OR Operator
+// if any one conditon matched then program will give true(1) output
+
 #include <stdio.h>
 
-int main(){
-    
+int main()
+{
+    int experince_year;
+    int project_countribuition;
+    printf("How many year have you experince in IT sector: ");
+    scanf("%d", &experince_year);
+    printf("How many open source project have you completed: ");
+    scanf("%d", &project_countribuition);
+    if ((experince_year >= 3 || project_countribuition >= 2))
+    {
+        printf("Congratulation! You are eligibal for applying in our interview round");
+    }
+    else
+    {
+        printf("Sorry you are not eligibal for our interview round");
+    }
     return 0;
 }
